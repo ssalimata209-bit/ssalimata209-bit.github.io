43127f15abfabe7cf4490a090581c8ca64af99a3
ssalimata209-bit <ssalimata209@gmail.com>
2026-07-01 08:40:56 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,10 @@
-// Configuration Firebase — NovaSpace
-// Projet : novaspace-f403a
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDu-N7o_6enI-vCqjKNsr1jkq",
+  apiKey: "AIzaSyDu-N7o_6enI-vCqjKNsr1jkq7sg5Rg_4A",
   authDomain: "novaspace-f403a.firebaseapp.com",
   projectId: "novaspace-f403a",
-  storageBucket: "novaspace-f403a.appspot.com",
+  storageBucket: "novaspace-f403a.firebasestorage.app",
   messagingSenderId: "118212233630",
-  appId: "1:118212233630:web:2c78a244a21c",
+  appId: "1:118212233630:web:2c78a244a21c1bcea4ee8f",
   measurementId: "G-PJD24XKHNY"
 };
 
